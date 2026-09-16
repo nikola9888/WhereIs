3178775025cfc24c8608a8ee33d4be1e4a8a27f9
nikola9888 <151091217+nikola9888@users.noreply.github.com>
2026-09-17 00:58:56 +0200

    Reduce Add Item screen button text size by 20%

--- screens/add_item.py
+++ screens/add_item.py
@@ -165,7 +165,7 @@ class AddItemScreen(Screen):
             background_down="",
             background_color=(0, 0, 0, 0),
             color=color,
-            font_size=84,
+            font_size=67.2,
             bold=bold
         )
 
